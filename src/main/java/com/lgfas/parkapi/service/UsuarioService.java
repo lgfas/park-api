695a2d2cf15fdef658679adfa0f5050e488e36ed
lgfas <luis.guilherme2811@gmail.com>
2024-03-14 12:02:17 -0300

    #F009 005 Recurso para localizar um usuário pelo id
    04 Recursos de Usuários

---
+++ src/main/java/com/lgfas/parkapi/service/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.lgfas.parkapi.service;
+
+import com.lgfas.parkapi.entity.Usuario;
+import com.lgfas.parkapi.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+    }
+}
