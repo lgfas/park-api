f8e08a2aa9c560ea7d47272c550921b75487afc8
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-03-18 10:46:54 -0300

    #F012 008 Vídeo explicativo para a próxima tarefa

--- src/main/java/com/lgfas/parkapi/service/UsuarioService.java
+++ src/main/java/com/lgfas/parkapi/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -22,5 +24,17 @@ public class UsuarioService {
         return usuarioRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Usuário não encontrado.")
         );
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }
--- src/main/java/com/lgfas/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/lgfas/parkapi/web/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/usuarios")
@@ -26,6 +28,20 @@ public class UsuarioController {
         Usuario user = usuarioService.buscarPorId(id);
 
         return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword (@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.editarSenha(id,usuario.getPassword());
+
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll () {
+        List<Usuario> users = usuarioService.buscarTodos();
+
+        return ResponseEntity.ok(users);
     }
 
 
