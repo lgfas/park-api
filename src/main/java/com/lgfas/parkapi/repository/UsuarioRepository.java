c9d6f9fd2bcf14d71a7c302a30def0046751280d
lgfas <luis.guilherme2811@gmail.com>
2024-03-22 12:05:19 -0300

    #F041 009 Ajustes finais para o processo de autenticação e login
    10  Sistema de Autenticação com Spring Security e JWT

--- src/main/java/com/lgfas/parkapi/repository/UsuarioRepository.java
+++ src/main/java/com/lgfas/parkapi/repository/UsuarioRepository.java
@@ -2,8 +2,15 @@ package com.lgfas.parkapi.repository;
 
 import com.lgfas.parkapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    @Query("select u.role from usuario u where u.username like :username")
+    Usuario.Role findRoleByUsername(String username);
 }
--- src/main/java/com/lgfas/parkapi/service/UsuarioService.java
+++ src/main/java/com/lgfas/parkapi/service/UsuarioService.java
@@ -7,6 +7,7 @@ import com.lgfas.parkapi.exception.UsernameUniqueViolationException;
 import com.lgfas.parkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +18,12 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado.", usuario.getUsername()));
@@ -44,16 +47,28 @@ public class UsuarioService {
 
         Usuario user = buscarPorId(id);
 
-        if (!user.getPassword().equals(senhaAtual)) {
+        if (!passwordEncoder.matches(senhaAtual, user.getPassword())) {
             throw new PasswordInvalidException("Sua senha não confere.");
         }
 
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
     @Transactional(readOnly = true)
     public List<Usuario> buscarTodos() {
         return usuarioRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário com username=%s não encontrado.", username))
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario.Role buscarRolePorUsername(String username) {
+        return usuarioRepository.findRoleByUsername(username);
     }
 }
