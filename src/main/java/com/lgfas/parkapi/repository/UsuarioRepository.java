fa254405966f3f7b8c5feff04dfe6fbe1a83c6fb
lgfas <luis.guilherme2811@gmail.com>
2024-03-25 10:26:09 -0300

    #F043 011 Habilitando o sistema de auditoria
    10  Sistema de Autenticação com Spring Security e JWT

--- src/main/java/com/lgfas/parkapi/repository/UsuarioRepository.java
+++ src/main/java/com/lgfas/parkapi/repository/UsuarioRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
 
-    @Query("select u.role from usuario u where u.username like :username")
+    @Query("select u.role from Usuario u where u.username like :username")
     Usuario.Role findRoleByUsername(String username);
 }
